0cb63d693f77018da8bb733552a177a97ef5cbd3
alexxxdev <alexxxdev@yandex.ru>
2018-11-09 18:44:11 +0700

    added websiteUrl in the user profile request

--- app/src/main/kotlin/com/github/alexxxdev/gitcat/data/GraphQLRepository.kt
+++ app/src/main/kotlin/com/github/alexxxdev/gitcat/data/GraphQLRepository.kt
@@ -17,21 +17,21 @@ class GraphQLRepository(
     fun getUserInfo(login: String): Result<GraphQLData<UserData>, FuelError> {
         val third = graphQLClient.getUserInfo("{\"query\": \"query {" +
                 "user(login: \\\"$login\\\") {" +
-                "id,login,name,avatarUrl,bio,company,email,location,resourcePath,url,updatedAt," +
-                "commitComments{totalCount}" +
-                "followers(first: 3){totalCount,nodes{login,name,avatarUrl}}" +
-                "following(first: 3){totalCount,nodes{login,name,avatarUrl}}" +
-                "gists{totalCount}" +
-                "gistComments{totalCount}" +
-                "watching{totalCount}" +
-                "issues(last: 3){totalCount,nodes{title,repository{id,name,description,nameWithOwner,url,createdAt,parent{id,name,description,nameWithOwner,url,createdAt}}}}" +
-                "issueComments{totalCount}" +
-                "starredRepositories(first: 3, orderBy: {field: STARRED_AT, direction: DESC}){totalCount,nodes{id,name,description,nameWithOwner,url,createdAt,parent{id,name,description,nameWithOwner,url,createdAt}}}" +
-                "pullRequests(last: 3){totalCount,nodes{title,body,repository{id,name,description,nameWithOwner,url,createdAt,parent{id,name,description,nameWithOwner,url,createdAt}}}}" +
-                "repositories(first: 3, orderBy: {field: CREATED_AT, direction: DESC}){totalCount,nodes{id,name,description,nameWithOwner,url,createdAt,parent{id,name,description,nameWithOwner,url,createdAt}}}" +
-                "repositoriesContributedTo(first: 3, orderBy: {field: CREATED_AT, direction: DESC}){totalCount,nodes{id,name,description,nameWithOwner,url,createdAt,parent{id,name,description,nameWithOwner,url,createdAt}}}" +
-                "organizations(first: 100){totalCount,nodes{id,login,name,avatarUrl,email,location,url}}" +
-                "pinnedRepositories(first: 3, orderBy: {field: CREATED_AT, direction: ASC}){totalCount,nodes{id,name,description,nameWithOwner,url,createdAt,parent{id,name,description,nameWithOwner,url,createdAt}}}" +
+                "id,login,name,avatarUrl,bio,company,email,location,resourcePath,url,updatedAt,websiteUrl," +
+                "commitComments{totalCount}," +
+                "followers(first: 3){totalCount,nodes{login,name,avatarUrl}}," +
+                "following(first: 3){totalCount,nodes{login,name,avatarUrl}}," +
+                "gists{totalCount}," +
+                "gistComments{totalCount}," +
+                "watching{totalCount}," +
+                "issues(last: 3){totalCount,nodes{title,repository{id,name,description,nameWithOwner,url,createdAt,parent{id,name,description,nameWithOwner,url,createdAt}}}}," +
+                "issueComments{totalCount}," +
+                "starredRepositories(first: 3, orderBy: {field: STARRED_AT, direction: DESC}){totalCount,nodes{id,name,description,nameWithOwner,url,createdAt,parent{id,name,description,nameWithOwner,url,createdAt}}}," +
+                "pullRequests(last: 3){totalCount,nodes{title,body,repository{id,name,description,nameWithOwner,url,createdAt,parent{id,name,description,nameWithOwner,url,createdAt}}}}," +
+                "repositories(first: 3, orderBy: {field: CREATED_AT, direction: DESC}){totalCount,nodes{id,name,description,nameWithOwner,url,createdAt,parent{id,name,description,nameWithOwner,url,createdAt}}}," +
+                "repositoriesContributedTo(first: 3, orderBy: {field: CREATED_AT, direction: DESC}){totalCount,nodes{id,name,description,nameWithOwner,url,createdAt,parent{id,name,description,nameWithOwner,url,createdAt}}}," +
+                "organizations(first: 100){totalCount,nodes{id,login,name,avatarUrl,email,location,url}}," +
+                "pinnedRepositories(first: 3, orderBy: {field: CREATED_AT, direction: ASC}){totalCount,nodes{id,name,description,nameWithOwner,url,createdAt,parent{id,name,description,nameWithOwner,url,createdAt}}}," +
                 "}" +
                 "}\"}").third
         authRepository.user = third.component1()?.data?.user
